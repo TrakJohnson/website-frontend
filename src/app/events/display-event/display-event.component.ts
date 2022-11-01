ceeb9570509305377d17285342600fba0db72a9a
Théo Takla <9513815+TrakJohnson@users.noreply.github.com>
2022-11-01 00:06:27 -0400

    fix emails and add names to admin event panel

--- src/app/events/display-event/display-event.component.ts
+++ src/app/events/display-event/display-event.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { ClipboardService } from 'ngx-clipboard'
-import { Event } from 'src/app/models/event.model';
-import { Place } from 'src/app/models/place.model';
-import { AccountService } from 'src/app/services/account.service';
-import { EventService } from 'src/app/services/events.service';
-import { PoleService } from 'src/app/services/poles.service';
-import { PopupService } from 'src/app/services/popup.service';
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+import {ClipboardService} from 'ngx-clipboard'
+import {Event} from 'src/app/models/event.model';
+import {Place} from 'src/app/models/place.model';
+import {AccountService} from 'src/app/services/account.service';
+import {EventService} from 'src/app/services/events.service';
+import {PoleService} from 'src/app/services/poles.service';
+import {PopupService} from 'src/app/services/popup.service';
 
 @Component({
   selector: 'app-display-event',
@@ -16,122 +16,132 @@ import { PopupService } from 'src/app/services/popup.service';
 })
 export class DisplayEventComponent implements OnInit {
 
-  constructor(private popup : PopupService,
-              private route : ActivatedRoute,
-              private router : Router,
-              private eventService : EventService,
-              private acc : AccountService,
-              private poleService : PoleService,
-              private _clipboardService: ClipboardService) { }
+  constructor(private popup: PopupService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private eventService: EventService,
+              private acc: AccountService,
+              private poleService: PoleService,
+              private _clipboardService: ClipboardService) {
+  }
 
-  event_id : number;
-  event : Event;
-  is_billetterie :boolean;
-  isAdmin : boolean | undefined = false;
-  placesAccepted : Place[] = [];
-  placesRejected : Place[] = [];
-  isAuth : boolean | undefined = false;
-  is_already_claimed : boolean  | undefined = false;
-  accountSub : Subscription;
-  on_sale : boolean;
-  pole :string
+  event_id: number;
+  event: Event;
+  is_billetterie: boolean;
+  isAdmin: boolean | undefined = false;
+  placesAccepted: Place[] = [];
+  placesRejected: Place[] = [];
+  isAuth: boolean | undefined = false;
+  is_already_claimed: boolean | undefined = false;
+  accountSub: Subscription;
+  on_sale: boolean;
+  pole: string;
+  usersInfos: any;
 
   ngOnInit(): void {
-    this.event_id = this.route.snapshot.params["event_id"] ;
+    this.event_id = this.route.snapshot.params["event_id"];
     this.eventService.getOneEvent(this.event_id)
-    .then((response) =>{
-      console.log(response.pole_id)
-      this.event = response;
-      this.placesAccepted = this.event.placesClaimed.filter((place) => place.status);
-      this.placesRejected = this.event.placesClaimed.filter((place) => !place.status);
-      this.is_billetterie = this.event.is_billetterie;
-      this.on_sale = this.event.on_sale;
-      this.pole = this.poleService.IDToPole.get(Number(this.event.pole_id))!
-      if (this.event.thumbnail == undefined || this.event.thumbnail.length < 1) {
-        this.event.thumbnail = "../../../assets/img/dev/default_event_pic.jpg"
-      }
-    })
-    .catch((error)=> {
-      this.popup.state$.next([false, error.message]);
-    })
+      .then((response) => {
+        console.log(response.pole_id)
+        this.event = response;
+        this.placesAccepted = this.event.placesClaimed.filter((place) => place.status);
+        this.placesRejected = this.event.placesClaimed.filter((place) => !place.status);
+        this.is_billetterie = this.event.is_billetterie;
+        this.on_sale = this.event.on_sale;
+        this.pole = this.poleService.IDToPole.get(Number(this.event.pole_id))!
+        if (this.event.thumbnail == undefined || this.event.thumbnail.length < 1) {
+          this.event.thumbnail = "../../../assets/img/dev/default_event_pic.jpg"
+        }
+        // participant infos
+        this.getInfosParticipants().then(x => {
+            this.usersInfos = Object.fromEntries(x.map((user: any) => [user.login, [user.prenom, user.nom]]));
+            console.log(this.usersInfos);
+          }
+        )
+      })
+      .catch((error) => {
+        this.popup.state$.next([false, error.message]);
+      })
 
     this.accountSub = this.acc.compte$.subscribe(
       (status) => {
         if (status) {
           this.isAdmin = status.admin;
           this.isAuth = status.login != "";
-          this.is_already_claimed = !status?.placesClaimed.every(place => {return place.event_id != this.event_id});
+          this.is_already_claimed = !status?.placesClaimed.every(place => {
+            return place.event_id != this.event_id
+          });
         }
-    });
+      });
 
     this.acc.getPlacesClaimedByUser();
   }
 
   buttonToDisplay() {
-    console.log({one : this.on_sale, two : this.on_sale});
+    console.log({one: this.on_sale, two: this.on_sale});
     if (!this.on_sale || !this.isAuth) {
       return 0
     } else {
-      console.log({claimed : this.is_already_claimed, out : this.is_already_claimed ? 1 : 2});
+      console.log({claimed: this.is_already_claimed, out: this.is_already_claimed ? 1 : 2});
       return this.is_already_claimed ? 2 : 1;
     }
   }
 
-  onClaimePlace(id_billetterie : number, size : number) {
+  onClaimePlace(id_billetterie: number, size: number) {
     this.acc.claimePlace(id_billetterie, size)
-    .then(() => {
-      this.popup.state$.next([true, "Demande enregistrée"]);
-      this.onNavigate('/events/display/' + this.event_id.toString());
-    })
-    .catch(() => {
-      this.popup.state$.next([false, "Erreur lors de la demande, veuillez contacter l'administrateur immédiatement"]);
-    })
+      .then(() => {
+        this.popup.state$.next([true, "Demande enregistrée"]);
+        this.onNavigate('/events/display/' + this.event_id.toString());
+      })
+      .catch(() => {
+        this.popup.state$.next([false, "Erreur lors de la demande, veuillez contacter l'administrateur immédiatement"]);
+      })
   }
 
-  onDeClaimePlace(id_billetterie : number) {
+  onDeClaimePlace(id_billetterie: number) {
     this.acc.declaimePlace(id_billetterie)
-    .then(() => {
-      this.popup.state$.next([true, "Demande retirée"]);
-      this.onNavigate('/events/display/' + this.event_id.toString());
-    })
-    .catch(() => {
-      this.popup.state$.next([false, "Erreur lors de la demande, veuillez contacter l'administrateur immédiatement"]);
-    })
+      .then(() => {
+        this.popup.state$.next([true, "Demande retirée"]);
+        this.onNavigate('/events/display/' + this.event_id.toString());
+      })
+      .catch(() => {
+        this.popup.state$.next([false, "Erreur lors de la demande, veuillez contacter l'administrateur immédiatement"]);
+      })
   }
 
   async onCloseBilletterie() {
     this.eventService.closeBilletterie(this.event_id)
-    .then(() => {
-      this.popup.state$.next([true, "Billetterie fermée avec succès"]);
-      this.onNavigate('/events/display/' + this.event_id.toString());
-    })
-    .catch(() => {
-      this.popup.state$.next([false, "Erreur lors de la fermeture, veuillez contacter l'administrateur immédiatement"]);
-    })
+      .then(() => {
+        this.popup.state$.next([true, "Billetterie fermée avec succès"]);
+        this.onNavigate('/events/display/' + this.event_id.toString());
+      })
+      .catch(() => {
+        this.popup.state$.next([false, "Erreur lors de la fermeture, veuillez contacter l'administrateur immédiatement"]);
+      })
   }
 
   async onReSaleBilletterie() {
     this.eventService.reSaleBilletterie(this.event_id)
-    .then(() => {
-      this.popup.state$.next([true, "Billetterie rouverte avec succès"]);
-      this.onNavigate('/events/display/' + this.event_id.toString());
-    })
-    .catch(() => {
-      this.popup.state$.next([false, "Erreur lors de la réouverture, veuillez contacter l'administrateur immédiatement"]);
-    })
+      .then(() => {
+        this.popup.state$.next([true, "Billetterie rouverte avec succès"]);
+        this.onNavigate('/events/display/' + this.event_id.toString());
+      })
+      .catch(() => {
+        this.popup.state$.next([false, "Erreur lors de la réouverture, veuillez contacter l'administrateur immédiatement"]);
+      })
   }
 
-  onGivePlace(place : Place) {
+  onGivePlace(place: Place) {
     this.eventService.givePlaceToUser(place.event_id, place.login)
     this.onNavigate('/events/display/' + this.event_id.toString());
   }
 
-  onRetirePlace(place : Place) {
+  onRetirePlace(place: Place) {
     this.eventService.retirePlaceToUser(place.event_id, place.login)
     this.onNavigate('/events/display/' + this.event_id.toString());
   }
 
-  copy(text: string){
+  copy(text: string) {
     this._clipboardService.copy(text)
   }
 
@@ -142,18 +152,21 @@ export class DisplayEventComponent implements OnInit {
 
   getEmailsParticipants() {
     /* Obtenir la liste des emails des participants, pour les membres gérant l'évènement */
-    this.getInfosParticipants()
-      .then(users => users.map((user : any) => user.email_mines ? user.email_mines : user.email))
-      .then((emails : string[]) => this.copy(emails.join("; ")));
+    // TODO: send an error when empty list, add confirmation when worked
+    if (this.placesAccepted.length > 0) {
+      this.getInfosParticipants()
+        .then(users => users.map((user: any) => user.email_mines ? user.email_mines : user.email))
+        .then((emails: string[]) => this.copy(emails.join("; ")));
+    }
   }
 
   onNavigate(endpoint: string) {
     // On navigate deux fois pour forcer le rechargement de la page
-    this.router.navigate(['/'], {skipLocationChange: true}).then(()=> this.router.navigate([endpoint]));
+    this.router.navigate(['/'], {skipLocationChange: true})
+      .then(() => this.router.navigate([endpoint]));
   }
 
   ngOnDestroy() {
     this.accountSub.unsubscribe()
   }
-
 }
